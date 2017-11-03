ba5be7dadc745845ebd40033332aa38f45396e84
Babith G <babithg@gmail.com>
2017-11-03 11:40:16 +0530

    Shell Script Uploaded
    Shell Script to copy all the files in a Directory to AWS S3 bucket with the help of aws cli command and bash shell on linux machine

---
+++ CopyLinuxtoAWSS3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Copy file to AWS S3 
+
+aws_path="s3://MyBucket/MyFolder/"
+localpath="/tmp/"
+errorReport="/tmp/S3_backuperror.txt"
+copyReport="/tmp/S3_copyreport.txt"
+echo "Script Started on :`date`">>$errorReport
+echo "Script Started on :`date`">>$copyReport
+
+for oneDB in `cat listDB.txt`
+do
+	abspath="$localpath$oneDB/"
+	awsabspath="$aws_path$(echo "$oneDB" | tr -d '[:space:]')/"
+
+	if [ -d $abspath ]
+	then
+		filecount=`ls $abspath | wc -l`
+		if [ $filecount -gt 0 ]
+		then
+			echo "Starting Copy to S3 from Directory:$abspath"
+			aws s3 cp $abspath $awsabspath --recursive
+
+			#Checking Copied or not 
+			for CheckingOne in `ls $abspath`
+			do
+				awssinglefile=$awsabspath$CheckingOne
+				WithAWS=`aws s3 ls $awssinglefile | awk '{print $3","$4}'`
+				localsinglefile=$abspath$CheckingOne
+				WithLocal=`ls -l $localsinglefile | awk '{print $5","}'`
+				WithLocal=$WithLocal$CheckingOne
+			        if [ "$WithAWS"<>"$WithLocal" ]
+				then
+					echo "SUCCESSFUL: Date:`date` File $localsinglefile Copied Successfully to $awssinglefile">>$copyReport
+				else
+					echo "ERROR: Date:`date` File $localsinglefile Not Copied to $awssinglefile">>$copyReport
+				fi
+			done
+
+		else
+			echo "No File Found on $abspath">>$errorReport
+		fi
+	else
+		echo "$oneDB not Found on $localpath">>$errorReport
+	fi
+
+done
